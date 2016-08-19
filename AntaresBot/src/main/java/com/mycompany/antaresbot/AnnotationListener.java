0120ed158eb05da53c3660473c734df8b3f94398
josefelipeq@live.com <josefelipeq@live.com>
2016-08-18 20:17:01 -0500

    Changes to pom.xml. Added user game status.

--- AntaresBot/src/main/java/com/mycompany/antaresbot/AnnotationListener.java
+++ AntaresBot/src/main/java/com/mycompany/antaresbot/AnnotationListener.java
@@ -9,8 +9,11 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import sx.blah.discord.api.events.Event;
 import sx.blah.discord.api.events.EventSubscriber;
+import sx.blah.discord.handle.impl.events.GameChangeEvent;
 import sx.blah.discord.handle.impl.events.MessageReceivedEvent;
 import sx.blah.discord.handle.impl.events.ReadyEvent;
+import sx.blah.discord.handle.impl.events.UserJoinEvent;
+import sx.blah.discord.handle.impl.events.UserLeaveEvent;
 import sx.blah.discord.util.DiscordException;
 import sx.blah.discord.util.MessageBuilder;
 import sx.blah.discord.util.MissingPermissionsException;
@@ -22,47 +25,72 @@ import sx.blah.discord.util.RequestBuffer;
  * @author JOSE QUIROGA
  */
 public class AnnotationListener {
-    
-    @EventSubscriber
-    public void onMessage(MessageReceivedEvent event){ 
-        RequestBuffer.request(()-> {
-            
-            System.out.println(event.getMessage().getChannel());
-        try {  
-            
-            if(!event.getMessage().getAuthor().getName().equals("jFluxie"))
-            {
-                String lastMessage=event.getMessage().getContent();
-                event.getMessage().delete();
-                new MessageBuilder(Bot.client).withChannel(event.getMessage().getChannel()).withContent("The following message has been deleted: '"+lastMessage+"' \n").build();
-                new MessageBuilder(Bot.client).withChannel(event.getMessage().getChannel()).withContent("http://i921.photobucket.com/albums/ad56/Trolling_is_a_art/U%20Mad/anime.jpg").build();
 
+    @EventSubscriber
+    public void onMessage(MessageReceivedEvent event) {
+        RequestBuffer.request(() -> {
+
+            try {
+
+                if (!event.getMessage().getContent().equals("!help")) {
+
+                    new MessageBuilder(Bot.client).withChannel(event.getMessage().getChannel()).withContent("Calmate bro.").build();
+
+                }
+
+            } catch (RateLimitException | DiscordException | MissingPermissionsException ex) {
+                Logger.getLogger(AnnotationListener.class.getName()).log(Level.SEVERE, null, ex);
             }
-            
-        } catch (RateLimitException | DiscordException | MissingPermissionsException ex) {
-            Logger.getLogger(AnnotationListener.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        
-        
+
         }
         );
-        
+
     }
-    
+
     @EventSubscriber
-    public void onReady(ReadyEvent event){ 
-        RequestBuffer.request(()-> {
-        
+    public void onReady(ReadyEvent event) {
+        RequestBuffer.request(() -> {
+
             try {
                 new MessageBuilder(Bot.client).withChannel("182651110756974592").withContent("@here Hello everyone my name is Antares, if you need anything from me type !help").build();
             } catch (RateLimitException | DiscordException | MissingPermissionsException ex) {
                 Logger.getLogger(AnnotationListener.class.getName()).log(Level.SEVERE, null, ex);
-                
+
             }
         }
         );
-    
+
+    }
+
+    @EventSubscriber
+    public void onChangeGame(GameChangeEvent event) {
+        String oldGame = event.getOldGame().toString().replace("Optional", "").replace(".", "").replace("[", "").replace("]", "");
+        String newGame = event.getNewGame().toString().replace("Optional", "").replace(".", "").replace("[", "").replace("]", "");
+
+        String message;
+
+        if (oldGame.equalsIgnoreCase("empty") && !newGame.equals("empty")) {
+            message = event.getUser() + " has entered " + newGame;
+
+        } else if (newGame.equalsIgnoreCase("empty") && !oldGame.equals("empty")) {
+            message = event.getUser() + " has left " + oldGame;
+
+        } else {
+            message = event.getUser() + " has changed from " + oldGame + " to " + newGame;
+
+        }
+
+        RequestBuffer.request(() -> {
+
+            try {
+                new MessageBuilder(Bot.client).withChannel("182651110756974592").withContent(message).build();
+            } catch (RateLimitException | DiscordException | MissingPermissionsException ex) {
+                Logger.getLogger(AnnotationListener.class.getName()).log(Level.SEVERE, null, ex);
+
+            }
+        }
+        );
+
     }
 
 }
-
--- AntaresBot/src/main/java/com/mycompany/antaresbot/Bot.java
+++ AntaresBot/src/main/java/com/mycompany/antaresbot/Bot.java
@@ -12,15 +12,34 @@ import sx.blah.discord.util.MessageBuilder;
 
 /**
  *
- * @author Jose
+ * @author JOSE QUIROGA
  */
 public class Bot {
-    
-    static IDiscordClient client;
-    
+
+    public static IDiscordClient client;
+
     public static void main(String[] args) throws DiscordException {
-        client = new ClientBuilder().withToken("MjE1ODc4MTk1NjUwNjI1NTM3.Cpd7xQ.Vm-ykhXZXig2IBSWISAuYSnipGk").login();
+        
+        client=getClient("MjE1ODc4MTk1NjUwNjI1NTM3.Cpd7xQ.Vm-ykhXZXig2IBSWISAuYSnipGk", false);
+        //client = new ClientBuilder().withToken("MjE1ODc4MTk1NjUwNjI1NTM3.Cpd7xQ.Vm-ykhXZXig2IBSWISAuYSnipGk").login();
+        client.login();
         client.getDispatcher().registerListener(new AnnotationListener());
     }
-    
+
+    /**
+     * Returns an instance of the discord client
+     */
+    public static IDiscordClient getClient(String token, boolean login) throws DiscordException { 
+        
+        ClientBuilder clientBuilder = new ClientBuilder(); //Creates the ClientBuilder instance
+        
+        clientBuilder.withToken(token); //Adds the login info to the builder
+        
+        if (login) {
+            return clientBuilder.login(); //Creates the client instance and logs the client in
+        } else {
+            return clientBuilder.build(); //Creates the client instance but it doesn't log the client in yet, you would have to call client.login() yourself
+        }
+    }
+
 }
